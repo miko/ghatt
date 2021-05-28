2269eec2667d28d04c6257d30254f6b25dd95cb2
Michał Kołodziejczyk <miko@wp.pl>
2021-05-28 10:01:16 +0200

    Added base image; figed waitforit dependency

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-TAG=v1.2.12
+TAG=v1.2.15
 docker build -t miko/ghatt .
 docker tag miko/ghatt miko/ghatt:${TAG}
+docker build --no-cache --rm -f Dockerfile.base -t miko/ghatt:base .
+docker tag miko/ghatt:base miko/ghatt:${TAG}-base
 
 
