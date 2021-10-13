238706d1c24b6749cb0fad3178aad8531ee66e8b
Michał Kołodziejczyk <miko@wp.pl>
2021-10-13 15:26:56 +0200

    Fix tag number; fix build script message

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ docker build --build-arg=TAG=${TAG} -t miko/ghatt .
 docker tag miko/ghatt miko/ghatt:${TAG}
 docker build --build-arg=TAG=${TAG} --no-cache --rm -f Dockerfile.base -t miko/ghatt:base .
 docker tag miko/ghatt:base miko/ghatt:${TAG}-base
-echo git push miko/ghatt:${TAG}
-echo git push miko/ghatt:latest
-echo git push miko/ghatt:base
-echo git push miko/ghatt:${TAG}-base
+echo docker push miko/ghatt:${TAG}
+echo docker push miko/ghatt:latest
+echo docker push miko/ghatt:base
+echo docker push miko/ghatt:${TAG}-base
 
